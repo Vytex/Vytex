3cec924efe2e498f39d8b03648f086d16844f6a1
smeggins <phillipchadwick@hotmail.com>
2021-01-31 16:49:35 -0800

    adds lines page html/css, dynamically genereates content on page using flask, changes heroimg and shadowbox styles to global, moves main flex styles from global to home.css

--- app.py
+++ app.py
@@ -7,5 +7,13 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route('/lineList')
+def lineList():
+    results = [
+        ["Test Venue", "8PM - LATE", ["9:15-9:45", "9:30-10:00", "10:15-10:45", "11:15-11:45"]]
+    ]
+    
+    return render_template("lineList.html", results=results)
+
 if  __name__ == "__main__":
     app.run(debug=True)
