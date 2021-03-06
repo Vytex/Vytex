dd714e8eea5e16da29abcaf140e3c48ea1aeb5b6
Julian Borderas-Ochoa <JulianBorderas-Ochoa@Julians-MacBook-Pro-2.local>
2021-03-05 19:43:43 -0800

    Adds UserAccount.html, UserLogin.html, their corresponding media & css files.

--- app.py
+++ app.py
@@ -5,7 +5,16 @@ app = Flask(__name__)
 # root to index
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template("Index.html")
+
+@app.route('/info')
+def info():
+    return render_template("UserAccount.html")
+
+@app.route('/login')
+def login():
+    return render_template("UserLogin.html")
+
 
 if  __name__ == "__main__":
     app.run(debug=True)
