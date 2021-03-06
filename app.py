324592791e18adf9cd88b39ca22e8246d0d92831
Joaquín Borderas Ochoa <joaquinborderasochoa@Joaquins-MacBook-Pro.local>
2021-03-05 18:12:04 -0800

    adds page contactus and createuser

--- app.py
+++ app.py
@@ -7,5 +7,14 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route('/contact')
+def contactus():
+    return render_template("contactus.html")
+
+@app.route('/create')
+def create():
+    return render_template("create.html")
+
+
 if  __name__ == "__main__":
     app.run(debug=True)
