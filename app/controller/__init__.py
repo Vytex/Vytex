5bccf11acf8b1d0280f4cbade60c29a887416d12
Karla Barrera <000428230@student.vcc.ca>
2021-03-18 12:23:02 -0700

    updates for userLines feature

--- app/controller/__init__.py
+++ app/controller/__init__.py
@@ -3,3 +3,4 @@ from app import app
 import app.controller.admin_controller
 import app.controller.home_controller
 import app.controller.lineList_controller
+import app.controller.userLines_controller
