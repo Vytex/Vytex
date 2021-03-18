f66e7acda38341e08d9e4206afbf7509f4373d35
Karla Barrera <000428230@student.vcc.ca>
2021-03-18 15:00:53 -0700

    userLines files added

---
+++ app/routes/userLines.py
@@ -0,0 +1,13 @@
+from random import randint
+
+from flask import request, redirect, url_for
+
+from app import app
+from app.controller.home_controller import home_controller
+from app.controller.userLines_controller import userLines_controller
+
+@app.route('/userLines')
+def userLines():
+    return userLines_controller.index()
+
+
