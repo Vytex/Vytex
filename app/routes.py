e8a0b74b192af5330ecd56c3fc9441b5c366b98e
smeggins <phillipchadwick@hotmail.com>
2021-03-19 18:39:30 -0700

    adds alt text to imgs

--- app/routes.py
+++
@@ -1,9 +0,0 @@
-# imports the "router"
-from app import app
-from routes import home
-from app.controller.home_controller import home_controller
-from app.controller.lineList_controller import lineList_controller
-from app.controller.admin_controller import admin_controller
-
-
-
