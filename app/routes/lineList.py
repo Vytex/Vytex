969b1824b1965b3c370b67ed99f16a95712d3b20
Julian Borderas-Ochoa <JulianBorderas-Ochoa@Julians-MacBook-Pro-2.local>
2021-03-22 17:44:45 -0700

    added authentication to linelist.py

--- app/routes/lineList.py
+++ app/routes/lineList.py
@@ -40,9 +40,15 @@ def lineList(venue):
 def profile():
     image_file = url_for('static', filename='assets/' + current_user.image_file)
     return render_template('Profile/profile.html', username=current_user.username, image_file=image_file)
+
 @app.route('/lineList/lineUp', methods = ['POST'])
 def lineUp():
     venueID = request.form["vID"]
     venueClose = request.form["vc"]
     lineTime = request.form.get("lineTimesS")
     return lineList_controller.lineUp(lineTime, venueID, venueClose)
+
+@authorization.route('/login', method=['GET'])
+@login_required
+def lineUpauth():
+    return render_template('Login/index.html')
